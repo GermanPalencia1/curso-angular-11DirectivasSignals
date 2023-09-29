0b72d633059805407ceb0e45ca368bd490b3d61f
Germán <german.palencia@adentia.es>
2023-09-29 14:18:36 +0200

    custome directive structure

---
+++ src/app/shared/directives/custom-label.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, ElementRef, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[customLabel]'
+})
+
+export class CustomLabelDirective  implements OnInit {
+
+  private htmlElement?: ElementRef<HTMLElement>;
+
+  constructor(private el: ElementRef<HTMLElement>) {
+    this.htmlElement = el;
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
