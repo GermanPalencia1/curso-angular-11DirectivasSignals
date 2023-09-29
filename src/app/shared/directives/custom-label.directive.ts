c9b54386cdbd1415c4141e50322f7be5e1caefa4
Germán <german.palencia@adentia.es>
2023-09-29 14:52:07 +0200

    change message label

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
   selector: '[customLabel]'
@@ -8,11 +9,17 @@ export class CustomLabelDirective  implements OnInit {
 
   private htmlElement?: ElementRef<HTMLElement>;
   private _color: string = 'red';
+  private _errors?: ValidationErrors | null;
 
   @Input() set color(value: string) {
     this._color = value;
     this.setStyle();
   }
+
+  @Input() set errors(value: ValidationErrors | null | undefined) {
+    this._errors = value;
+    this.setErrorMessage();
+ }
 
   constructor(private el: ElementRef<HTMLElement>) {
     this.htmlElement = el;
@@ -25,6 +32,23 @@ export class CustomLabelDirective  implements OnInit {
   setStyle():void {
     if(!this.htmlElement) return;
     this.htmlElement!.nativeElement.style.color = this._color;
+  }
+
+  setErrorMessage(): void {
+
+    if(!this.htmlElement) return;
+
+    if(!this._errors) {
+      this.htmlElement.nativeElement.innerText = 'No hay errores';
+      return;
+    }
+
+    const errors = Object.keys(this._errors);
+
+    if(errors.includes('required')) {
+      this.htmlElement.nativeElement.innerText = 'Este campo es requerido';
+      return;
+    }
   }
 
 }
